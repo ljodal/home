0e51166d1cbb8d61086b5845c92c89af8c92ae8c
Sigurd Ljødal <sigurd@ljodal.no>
2020-01-05 19:15:18 +0100

    Format migrations with pgformatter

--- migrations/0001_initial.sql
+++ migrations/0001_initial.sql
@@ -4,17 +4,14 @@ create table device (
     description text not null
 );
 
-
 create table sensor_reading (
     device_id integer references device (id) not null,
     timestamp timestamptz not null,
     temperature real,
     relative_humidity real,
-
     -- Validate that at least one sensor reading is set
-    constraint sensor_reading_has_value
-    check (num_nonnulls(temperature, relative_humidity) > 0),
-
+    constraint sensor_reading_has_value check (num_nonnulls (temperature,
+            relative_humidity) > 0),
     -- Make sure device readings have a unique timestamp
-    unique ( device_id, timestamp )
+    unique (device_id, timestamp)
 );
